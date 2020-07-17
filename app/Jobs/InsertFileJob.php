14b0310cb2da7c419fe7c8bbccf8087ab76e15cb
Maycon Paiva <mayconpaivac@gmail.com>
2020-07-17 19:31:05 -0400

    add convert coodinate geographic to utm if is geographic

--- app/Jobs/InsertFileJob.php
+++ app/Jobs/InsertFileJob.php
@@ -3,6 +3,8 @@
 namespace App\Jobs;
 
 use App\Immobile;
+use App\Support\Decimal;
+use App\Support\GPointConverter;
 use App\Vertice;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -46,6 +48,8 @@ class InsertFileJob implements ShouldQueue
             while (($line = fgetcsv($handle, 100000000, ';')) !== false) {
                 yield $line;
             }
+
+            fclose($handle);
         });
 
         foreach ($csv as $key => $line) {
@@ -65,18 +69,45 @@ class InsertFileJob implements ShouldQueue
             while (($line = fgetcsv($handle, 10000000, ';')) !== false) {
                 yield $line;
             }
+
+            fclose($handle);
         });
 
         foreach ($csv as $key => $line) {
             if ($key !== 0) {
+                $este = str_replace(',', '.', $line[9]);
+                $norte = str_replace(',', '.', $line[10]);
+
+                if (strstr($este, 'W')) {
+                    $decimal = new Decimal();
+                    $x = explode(' ', str_replace('W', '', $este));
+                    $deg = $x[0];
+                    $min = $x[1];
+                    $sec = str_replace(',', '.', $x[2]);
+
+                    $longitude = '-' . $decimal->DMStoDEC($deg, $min, $sec);
+
+                    $x = explode(' ', str_replace('S', '', $norte));
+                    $deg = $x[0];
+                    $min = $x[1];
+                    $sec = str_replace(',', '.', $x[2]);
+
+                    $latitude = '-' . $decimal->DMStoDEC($deg, $min, $sec);
+
+                    $gpoint = new GPointConverter('WGS 84');
+                    $gpoint = $gpoint->convertLatLngToUtm($latitude, $longitude);
+                    $este = $gpoint[0];
+                    $norte = $gpoint[1];
+                }
+
                 $data['vertices'][$key - 1]['immobile_id'] = $immobile->id;
                 $data['vertices'][$key - 1]['vertice'] = $line[1];
                 $data['vertices'][$key - 1]['sigma_x'] = str_replace(',', '.', $line[4]);
                 $data['vertices'][$key - 1]['sigma_y'] = str_replace(',', '.', $line[5]);
                 $data['vertices'][$key - 1]['sigma_z'] = str_replace(',', '.', $line[6]);
                 $data['vertices'][$key - 1]['indice'] = $line[8];
-                $data['vertices'][$key - 1]['este'] = str_replace(',', '.', $line[9]);
-                $data['vertices'][$key - 1]['norte'] = str_replace(',', '.', $line[10]);
+                $data['vertices'][$key - 1]['este'] = $este;
+                $data['vertices'][$key - 1]['norte'] = $norte;
                 $data['vertices'][$key - 1]['altura'] = str_replace(',', '.', $line[11]);
             }
         }
@@ -87,7 +118,7 @@ class InsertFileJob implements ShouldQueue
 
         DB::commit();
 
-        Storage::delete('download/parcela_' . $this->code);
-        Storage::delete('download/vertices_' . $this->code);
+        Storage::delete('download/parcela_' . $this->code . '.csv');
+        Storage::delete('download/vertices_' . $this->code . '.csv');
     }
 }
