5dbb23be45524c0af0b1ba7d676dcc9650c4ebf0
Maycon Paiva <mayconpaivac@gmail.com>
2020-07-19 10:57:43 -0400

    updates

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
     public function init()
     {
         $lines = LazyCollection::make(function () {
-            $handle = Storage::disk('spaces')->readStream('app/codes.txt');
+            $handle = Storage::disk('spaces')->readStream('codes.txt');
 
             while (($line = fgets($handle)) !== false) {
                 yield $line;
